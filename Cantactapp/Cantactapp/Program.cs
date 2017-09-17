3ae6ca7e0c6e850aa4610f0ce123a743b15392bd
nhan181197 <nhan181197@gmail.com>
2017-09-17 13:28:47 +0700

    mã nguồn của ngày đầu tiên
    tạo lớp Contact và lớp Program

---
+++ Cantactapp/Cantactapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cantactapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> danhba = new List<Contact>();
+            char r;
+            do
+            {
+                Console.WriteLine("\nMoi nhap thong tin lien lac:");
+                Console.Write("Moi nhap ho:");
+                string ho = Console.ReadLine();
+                Console.Write("Moi nhap ten:");
+                string ten = Console.ReadLine();
+                Console.Write("Moi nhap dia chi:");
+                string diachi = Console.ReadLine();
+                Console.Write("Moi nhap so dien thoai:");
+                string sdt = Console.ReadLine();
+
+                Contact co = new Contact(ho, ten, diachi, sdt);
+                danhba.Add(co);
+
+                Console.Write("Ban co muon tiep tuc nhap hay khong (y/n) ? ");
+                r = Console.ReadKey().KeyChar;
+            } while (r == 'y');
+
+            Console.WriteLine("\n\nDanh ba da nhap gom:");
+            foreach (Contact c in danhba)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}",
+                                 c.GetHo(),
+                                 c.GetTen(),
+                                 c.GetDiaChi(),
+                                 c.GetSDT());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cantactapp/Cantactapp/contact.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cantactapp
+{
+    class contact
+    {
+        string ho;
+  string ten;
+  string diachi;
+  string sdt;
+
+  public Contact(string ho, string ten, string diachi, string sdt)
+  {
+      this.ho = ho;
+      this.ten = ten;
+      this.diachi = diachi;
+      this.sdt = sdt;
+  }
+
+  public void SetHo(string ho)
+  {
+      this.ho = ho;
+  }
+
+  public string GetHo()
+  {
+      return this.ho;
+  }
+
+  public void SetTen(string ten)
+  {
+      this.ten = ten;
+  }
+
+  public string GetTen()
+  {
+      return this.ten;
+  }
+
+  public void SetDiaChi(string diachi)
+  {
+      this.diachi = diachi;
+  }
+
+  public string GetDiaChi()
+  {
+      return this.diachi;
+  }
+
+  public void SetSDT(string sdt)
+  {
+      this.sdt = sdt;
+  }
+
+  public string GetSDT()
+  {
+      return this.sdt;
+  }
+    }
+}
